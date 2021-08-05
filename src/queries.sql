1384e6d28af322120dc7a130f7ec419842fe758f
reidtc82 <reidtc82@gmail.com>
2021-08-04 22:50:14 -0700

    writing queries

--- src/queries.sql
+++ src/queries.sql
@@ -70,43 +70,115 @@ LIMIT 1;
 -- player_booked -> match_no, team_id, player_id
 -- player_mast -> player_id, player_name, jersey_no
 -- 1208, G
-
+SELECT DISTINCT player_mast.player_name, 
+    player_mast.jersey_no
+FROM player_mast
+INNER JOIN match_details
+ON player_mast.team_id = match_details.team_id
+WHERE posi_to_play = 'GK' AND 
+    player_mast.team_id = 1208 AND
+    match_details.play_stage = 'G';
 
 --- 10. all available information about the players under contract to
 ---    Liverpool F.C. playing for England in EURO Cup 2016.
-
+SELECT DISTINCT pm.*, pio.*, pb.*, gd.*, ps.*
+FROM player_mast AS pm
+LEFT JOIN player_in_out AS pio 
+    ON pm.player_id = pio.player_id
+LEFT JOIN player_booked AS pb
+    ON pm.player_id = pb.player_id
+LEFT JOIN goal_details AS gd
+    ON pm.player_id = gd.player_id
+LEFT JOIN penalty_shootout AS ps
+    ON pm.player_id = ps.player_id
+WHERE pm.playing_club LIKE 'Liverpool%' AND
+    pm.team_id = 1206;
 
 --- 11. the players, their jersey number, and playing club who were
 ---     the goalkeepers for England in EURO Cup 2016.
-
+SELECT player_name, jersey_no, playing_club
+FROM player_mast
+WHERE team_id = 1206 AND posi_to_play = 'GK';
 
 --- 12. the total number of goals scored by each position on
 ---     each country’s team. Do not include positions which scored no goals.
-
+SELECT COUNT(goal_id) AS goal_count,
+    pm.posi_to_play,
+    pm.team_id
+FROM goal_details AS gd
+INNER JOIN player_mast AS pm
+ON gd.player_id = pm.player_id
+GROUP BY pm.posi_to_play, pm.team_id;
 
 --- 13. all the defenders who scored a goal for their teams.
-
+--- DF
+SELECT DISTINCT pm.player_id, pm.player_name
+FROM goal_details gd
+INNER JOIN player_mast pm
+ON pm.player_id = gd.player_id
+WHERE pm.posi_to_play = 'DF';
 
 --- 14. referees and the number of bookings they made for the
 ---     entire tournament. Sort your answer by the number of bookings in descending order.
-
+SELECT COUNT(pb.match_no) AS bookings,
+    rm.referee_name
+FROM player_booked pb
+INNER JOIN match_mast mm
+    ON pb.match_no = mm.match_no
+INNER JOIN referee_mast rm
+    ON mm.referee_id = rm.referee_id
+GROUP BY rm.referee_name
+ORDER BY COUNT(pb.match_no) DESC;
 
 --- 15. the referees who booked the most number of players.
-
+SELECT 
+    COUNT(pb.player_id) AS players_booked,
+    mm.referee_id
+FROM player_booked pb
+INNER JOIN match_mast mm
+    USING(match_no)
+GROUP BY mm.referee_id
+ORDER BY COUNT(pb.player_id) DESC
+LIMIT 1;
 
 --- 16. referees and the number of matches they worked in each
 ---     venue.
-
+SELECT rm.referee_name,
+    sv.venue_name,
+    COUNT(match_no) AS worked
+FROM match_mast mm 
+INNER JOIN referee_mast rm ON mm.referee_id = rm.referee_id 
+INNER JOIN soccer_venue sv ON mm.venue_id = sv.venue_id
+GROUP BY rm.referee_name, sv.venue_name;
 
 --- 17. the country where the most assistant referees come from,
 ---     and the count of the assistant referees.
-
+SELECT COUNT(am.ass_ref_id) AS assistants,
+    sc.country_name
+FROM asst_referee_mast am
+INNER JOIN soccer_country sc ON am.country_id = sc.country_id
+GROUP BY sc.country_name
+ORDER BY COUNT(am.ass_ref_id) DESC
+LIMIT 1;
 
 --- 18. the highest number of foul cards given in one match.
-
+SELECT COUNT(player_id) bookings,
+    match_no
+FROM player_booked
+GROUP BY match_no
+ORDER BY COUNT(player_id) DESC
+LIMIT 1;
 
 --- 19. the number of captains who were also goalkeepers.
-
+SELECT COUNT(player_captain) AS capt_gk
+FROM match_captain mc
+INNER JOIN player_mast pm ON mc.player_captain = pm.player_id
+WHERE pm.posi_to_play = 'GK';
 
 --- 20. the substitute players who came into the field in the first
 ---     half of play, within a normal play schedule.
+--- in_out = I
+SELECT pm.player_name
+FROM player_in_out pio
+INNER JOIN player_mast pm ON pio.player_id = pm.player_id
+WHERE in_out = 'I' AND play_half < 2 AND play_schedule = 'NT';
